238098623c43d904f54c7bd787e8e8c17234830a
cos <deniz@denizs-MacBook-Air.local>
2017-06-05 01:10:58 +0300

    api removed

--- src/app/services/nutrition.service.ts
+++ src/app/services/nutrition.service.ts
@@ -12,7 +12,7 @@ export class NutritionService {
   apiKey: string;
 
   constructor(private http: Http) { 
-    this.apiKey = 'KxhNfT3pMwlnn21HUCCs61iG2JwT9EmMrgOnOiU7';
+    this.apiKey = 'YOUR_API_KEY';
 
   }
 
